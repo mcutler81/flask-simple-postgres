2486e7ceb4e2317b33b6d80f4d104347bdc0fa6d
mcutler81 <matthewacutler@gmail.com>
2023-05-30 19:23:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,31 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://matthewacutler:NU2bI6PoEeKs@ep-shiny-mouse-158204.us-east-2.aws.neon.tech/sarah-profile'
 db = SQLAlchemy(app)
 
-class BlogPost(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return 'Blog post ' + str(self.id)
+    photo = db.Column(db.String(200), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    dob = db.Column(db.DateTime, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    phone_number = db.Column(db.String(20), nullable=False)
+    account_number = db.Column(db.String(20), nullable=False)
+    notes = db.Column(db.Text)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
-@app.route('/posts', methods=['GET', 'POST'])
-def posts():
-
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        post_author = request.form['author']
-        new_post = BlogPost(title=post_title, content=post_content, author=post_author) 
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
-        return render_template('posts.html', posts=all_posts)
-
-@app.route('/posts/delete/<id>')
-def delete(id):
-    post = BlogPost.query.get_or_404(id)
-    db.session.delete(post)
+@app.route('/update_notes', methods=['POST'])
+def update_notes():
+    user_id = request.form.get('user_id')
+    notes = request.form.get('notes')
+    user = User.query.get(user_id)
+    user.notes = notes
     db.session.commit()
-    return redirect('/posts')
-
-@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-
-    post = BlogPost.query.get_or_404(id)
-
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.author = request.form['author']
-        post.content = request.form['content']
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('edit.html', post=post)
-
-@app.route('/posts/new', methods=['GET', 'POST'])
-def new_post():
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.author = request.form['author']
-        post.content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('new_post.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return redirect('/')
